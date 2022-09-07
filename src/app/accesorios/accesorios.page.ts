539e343111ad3ab5172e29492ad2e1e4651bb7ed
jossaldias <101900851+jossaldias@users.noreply.github.com>
2022-09-06 23:33:01 -0300

    login accesorios

--- src/app/accesorios/accesorios.page.ts
+++ src/app/accesorios/accesorios.page.ts
@@ -8,23 +8,23 @@ import { Router } from '@angular/router';
 })
 export class AccesoriosPage implements OnInit {
 
- 
 
-  public listarAccesorios =[{
-    "nombreA":"Audifonos Razer Black",
-    "precioA":55990,
-    "imagenA":"audifonosrazer.png",
-    "descripcionA":"¡Experimenta la adrenalina de sumergirte en la escena de otra manera! Tener auriculares específicos para jugar cambia completamente tu experiencia en cada partida. Con los Razer Kraken X Lite no te pierdes ningún detalle y escuchas el audio tal y como fue diseñado por los creadores"
+
+  public listarAccesorios = [{
+    "nombreA": "Audifonos Razer Black",
+    "precioA": 55990,
+    "imagenA": "audifonosrazer.png",
+    "descripcionA": "¡Experimenta la adrenalina de sumergirte en la escena de otra manera! Tener auriculares específicos para jugar cambia completamente tu experiencia en cada partida. Con los Razer Kraken X Lite no te pierdes ningún detalle y escuchas el audio tal y como fue diseñado por los creadores"
 
   },
   {
-    "nombreA":"Audífonos in-ear inalámbricos Baseus W04 Pro black",
-    "precioA":30990,
-    "imagenA":"audifonosoidos.png",
-    "descripcionA":"En la calle, en el colectivo o en la oficina, ten siempre a mano tus audífonos Baseus y ¡escápate de la rutina por un rato! Vas a poder disfrutar de la música que más te gusta y de tus podcasts favoritos cuando quieras y donde quieras."
+    "nombreA": "Audífonos in-ear inalámbricos Baseus W04 Pro black",
+    "precioA": 30990,
+    "imagenA": "audifonosoidos.png",
+    "descripcionA": "En la calle, en el colectivo o en la oficina, ten siempre a mano tus audífonos Baseus y ¡escápate de la rutina por un rato! Vas a poder disfrutar de la música que más te gusta y de tus podcasts favoritos cuando quieras y donde quieras."
 
   }
-];
+  ];
 
   constructor(private router: Router) { }
 
@@ -35,7 +35,7 @@ export class AccesoriosPage implements OnInit {
   }
 
   verAccesorios(nombreA: String, precioA: Number, imagenA: String, descripcionA: String) {
-    let url = `accesorios/${nombreA}/${precioA}/${imagenA}/${descripcionA}`;
+    let url = `productoaccesorios/${nombreA}/${precioA}/${imagenA}/${descripcionA}`;
 
     this.router.navigate([url])
     console.log(url)
--- src/app/juegos/juegos.page.ts
+++ src/app/juegos/juegos.page.ts
@@ -8,38 +8,38 @@ import { Router } from '@angular/router';
 })
 export class JuegosPage implements OnInit {
 
-  public listarJuegos=[
+  public listarJuegos = [
     {
-      "nombreJ":"Skyrim",
-      "precioJ":29990,
-      "imagenJ":"juego1.png",
-      "descripcionJ":"Juego para niñasThe Elder Scrolls V: Skyrim es un ARPG del tipo mundo abierto desarrollado por Bethesda Game Studios y publicado por Bethesda Softworks."
+      "nombreJ": "Skyrim",
+      "precioJ": 29990,
+      "imagenJ": "juego1.png",
+      "descripcionJ": "Juego para niñasThe Elder Scrolls V: Skyrim es un ARPG del tipo mundo abierto desarrollado por Bethesda Game Studios y publicado por Bethesda Softworks."
     },
     {
-      "nombreJ":"Lego STAR WARS",
-      "precioJ":29990,
-      "imagenJ":"juego2.png",
-      "descripcionJ":"Lego Star Wars: The Skywalker Saga es un videojuego de Lego del género acción-aventura desarrollado por TT Games y distribuido por Warner Bros."
+      "nombreJ": "Lego STAR WARS",
+      "precioJ": 29990,
+      "imagenJ": "juego2.png",
+      "descripcionJ": "Lego Star Wars: The Skywalker Saga es un videojuego de Lego del género acción-aventura desarrollado por TT Games y distribuido por Warner Bros."
     },
     {
-      "nombreJ":"Animal Crossing",
-      "precioJ":29990,
-      "imagenJ":"juego3.png",
-      "descripcionJ":"Animal Crossing: New Horizons es un videojuego de simulación social desarrollado y publicado por Nintendo para Nintendo Switch, cuya fecha de lanzamiento mundial fue el 20 de marzo de 2020. Es la novena entrega de la saga Animal Crossing."
+      "nombreJ": "Animal Crossing",
+      "precioJ": 29990,
+      "imagenJ": "juego3.png",
+      "descripcionJ": "Animal Crossing: New Horizons es un videojuego de simulación social desarrollado y publicado por Nintendo para Nintendo Switch, cuya fecha de lanzamiento mundial fue el 20 de marzo de 2020. Es la novena entrega de la saga Animal Crossing."
     },
     {
-      "nombreJ":"DonkeyKong",
-      "precioJ":29990,
-      "imagenJ":"juego4.png",
-      "descripcionJ":"Donkey Kong Country: Tropical Freeze ya se está preparando para el debut de la serie en Nintendo Switch!"
+      "nombreJ": "DonkeyKong",
+      "precioJ": 29990,
+      "imagenJ": "juego4.png",
+      "descripcionJ": "Donkey Kong Country: Tropical Freeze ya se está preparando para el debut de la serie en Nintendo Switch!"
     },
     {
-      "nombreJ":"Mario",
-      "precioJ":29990,
-      "imagenJ":"juego5.png",
-      "descripcionJ":"Super Mario Odyssey es un videojuego de plataformas en tres dimensiones para Nintendo Switch desarrollado y publicado por Nintendo que se lanzó el 27 de octubre de 2017"
+      "nombreJ": "Mario",
+      "precioJ": 29990,
+      "imagenJ": "juego5.png",
+      "descripcionJ": "Super Mario Odyssey es un videojuego de plataformas en tres dimensiones para Nintendo Switch desarrollado y publicado por Nintendo que se lanzó el 27 de octubre de 2017"
     }
-    
+
   ]
 
   constructor(private router: Router) { }
@@ -47,10 +47,10 @@ export class JuegosPage implements OnInit {
 
   ngOnInit() {
   }
-verJuegos(nombreJ:String,precioJ:Number,imagenJ:String,descripcionJ:String){
-  let url = `juegos/${nombreJ}/${precioJ}/${imagenJ}/${descripcionJ}`;
+  verJuegos(nombreJ: String, precioJ: Number, imagenJ: String, descripcionJ: String) {
+    let url = `producto-juegos/${nombreJ}/${precioJ}/${imagenJ}/${descripcionJ}`;
 
-  this.router.navigate([url])
+    this.router.navigate([url])
     console.log(url)
-}
+  }
 }
